b48e1214c6b8ce50c8d35ec23cbd6b56fa4ed9b6
charitha3006 <nomenclaturetaurus@gmail.com>
2025-02-03 11:25:26 +0530

    Day 2

---
+++ wipro_java_project/src/com/wipro/java/oops/Employee.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package com.wipro.java.oops;
+
+/**
+ * 
+ */
+public class Employee {
+
+	/**
+	 * 
+	 */
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	private String employeeName;
+	private int employeeId;
+	private String empEmail;
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getEmpEmail() {
+		return empEmail;
+	}
+
+	public void setEmpEmail(String empEmail) {
+		this.empEmail = empEmail;
+	}
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
