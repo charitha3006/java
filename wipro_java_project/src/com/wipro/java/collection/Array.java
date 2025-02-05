0ef8f9d0ec466029571718fe8bc4a4338cf87bc7
charitha3006 <nomenclaturetaurus@gmail.com>
2025-02-05 14:40:02 +0530

    Day 4

---
+++ wipro_java_project/src/com/wipro/java/collection/Array.java
@@ -0,0 +1,20 @@
+package com.wipro.java.collection;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr;;
+		arr=new int[5];
+		arr[0]=1;
+		arr[1]=2;
+		arr[2]=3;
+		arr[3]=4;
+		arr[4]=5;
+		for(int i=0;i<5;i++) {
+			System.out.println(arr[i]);
+		}
+
+	}
+
+}
